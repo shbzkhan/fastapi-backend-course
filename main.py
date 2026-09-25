c6c347fa2ef32c097e644f157a6af12aef6a0730
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2026-09-25 23:17:37 +0530

    learned params

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from mockData import products
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message":"Hello world form FastAPI VENV"}
+
+@app.get("/products")
+def products_data():
+    return products
+
+# body prams
+@app.get("/product/{product_id}")
+def product_by_id(product_id:int):
+    for productOne in products:
+        if productOne.get("id") == product_id:
+            return productOne
+    return{
+        "error":"Product not found"
+    }
+
+#query params
+@app.get("/greet")
+def greet(name:str, age:str):
+    return{
+        "data":f"Hello {name}, Your age is {age}"
+    }
+
+@app.get("/greet-request")
+def greetRequest(request:Request):
+    query_params = dict(request.query_params)
+    return{
+        "data":f"Hello {query_params.get("name")}, Your age is {query_params.get("age")}"
+    }
---
+++ mockData.py
@@ -0,0 +1,13 @@
+products = [
+    {
+        "id":1,
+        "name":"kaise ho",
+        "product":"products_one"
+        
+    },
+    {
+        "id":2,
+        "name":"kaise ho",
+        "product":"products_two"
+    }
+]
